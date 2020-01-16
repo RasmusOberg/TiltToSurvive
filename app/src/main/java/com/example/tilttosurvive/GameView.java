57fdd048efcaf2b0f3ca7f2a0f440473bf39c4fb
Rasmus Oberg <rasmus.oberg@hotmail.com>
2020-01-16 12:49:04 +0100

    Fixed timer

--- app/src/main/java/com/example/tilttosurvive/GameView.java
+++ app/src/main/java/com/example/tilttosurvive/GameView.java
@@ -171,11 +171,12 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
             gameActivity.unRegisterSensors();
             AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
             builder.setTitle("Highscore! =D Enter Name");
+            builder.setMessage("You're time is = " + time10th);
+            showTimer();
 
             ArrayList<Score> list = (ArrayList<Score>) repo.getHighscores();
             Log.w("TEST123","SCORE: " + time10th  + ", double value: " + Double.parseDouble(time10th));
             Log.w("TEST123","List(2) " + list.get(2).getScore());
-
 
 //            if (list.get(2).getScore() > Double.parseDouble(time10th)) {
                 final EditText input = new EditText(getContext());
